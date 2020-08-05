d4ecdded9cdd188a9e0bc6996207a86ab3372ed4
Felipe Fonseca <fonsecafel@gmail.com>
2020-08-05 01:40:56 -0300

    Finish landing page and teacher list page

---
+++ src/components/TeacherItem/index.tsx
@@ -0,0 +1,44 @@
+import React from 'react';
+
+import whatsappIcon from '../../assets/images/icons/whatsapp.svg';
+
+import './styles.css';
+
+const TeacherItem: React.FC = () => {
+  return (
+    <article className="teacher-item">
+      <header>
+        <img
+          src="https://avatars1.githubusercontent.com/u/2052767?s=400&u=55ece77c1db891da58f7f02c6a3c3970ecaef6ab&v=4"
+          alt="Felipe"
+        />
+        <div>
+          <strong>Felipe Fonseca</strong>
+          <span>Química</span>
+        </div>
+      </header>
+
+      <p>
+        Entusiasta das melhores tecnologias de química avançada.
+        <br />
+        <br />
+        Apaixonado por explodir coisas em laboratório e por mudar a vida das
+        pessoas através de experiências. Mais de 200.000 pessoas já passaram por
+        uma das minhas explosões.
+      </p>
+
+      <footer>
+        <p>
+          Preço/hora
+          <strong>R$ 80,00</strong>
+        </p>
+        <button type="button">
+          <img src={whatsappIcon} alt="Whatsapp" />
+          Entrar em contato
+        </button>
+      </footer>
+    </article>
+  );
+};
+
+export default TeacherItem;
--- src/pages/Landing/index.tsx
+++ src/pages/Landing/index.tsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 import logoImg from '../../assets/images/logo.svg';
 import landingImage from '../../assets/images/landing.svg';
@@ -25,15 +26,15 @@ const Landing: React.FC = () => {
         />
 
         <div className="buttons-container">
-          <a href="test" className="study">
+          <Link to="/study" className="study">
             <img src={studyIcon} alt="Estudar" />
             Estudar
-          </a>
+          </Link>
 
-          <a href="test" className="give-classes">
-            <img src={giveClassesIcon} alt="Dar aula" />
-            Dar aula
-          </a>
+          <Link to="/give-classes" className="give-classes">
+            <img src={giveClassesIcon} alt="Dar aulas" />
+            Dar aulas
+          </Link>
         </div>
 
         <span className="total-connections">
